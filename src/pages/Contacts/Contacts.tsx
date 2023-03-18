f4cbe3339dad83669a3e5a97f0e362d952887d86
Dani <daniyil.kud@gmail.com>
2023-03-18 19:05:09 +0100

    add map on contact page

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -4,19 +4,23 @@ import "./Contacts.scss"
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import LocalPostOfficeOutlinedIcon from '@mui/icons-material/LocalPostOfficeOutlined';
 import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import Map from "components/Map/Map";
 
 type Props = {}
 const Contacts = ({}: Props) => {
-    return <>
+    return <div>
     <Container>
         <PageTitle title="I nostri Contatti"/>
         <Typography textAlign="center" variant="h4" component="h4">SLAVA UKRAINI ODV</Typography>
-        <ul className="contacts">
-            <li className="address"><LocationOnOutlinedIcon fontSize="small"/> CORSO DEL POPOLO 84 45100 ROVIGO Italia</li>
-            <li className="email"><LocalPostOfficeOutlinedIcon fontSize="small"/> slavaukrainiodv@gmail.com</li>
-            <li className="phone"><LocalPhoneOutlinedIcon fontSize="small"/> +39 380 791 4144</li>
-        </ul>
+        <div className="contacts">
+            <Map />
+            <ul className="contacts-list">
+                <li className="address"><LocationOnOutlinedIcon fontSize="small"/> CORSO DEL POPOLO 84 45100 ROVIGO Italia</li>
+                <li className="email"><LocalPostOfficeOutlinedIcon fontSize="small"/> slavaukrainiodv@gmail.com</li>
+                <li className="phone"><LocalPhoneOutlinedIcon fontSize="small"/> +39 380 791 4144</li>
+            </ul>
+        </div>
     </Container>
-    </>
+    </div>
 }
 export default Contacts
